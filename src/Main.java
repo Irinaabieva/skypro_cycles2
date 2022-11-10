23e29150d8d05056a816dd39ff358ac211ce7d9e
VarzievE <edvarziev@gmail.com>
2022-11-10 17:37:40 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,9 @@ public class Main {
         for (int i = 1904; i <= 2096; i += 4) {
             System.out.println(i + " год является весокосным");
         }
+        //Задание 2
+        for (int i = 7; i <= 100; i += 7) {
+            System.out.println(i);
+        }
     }
 }
